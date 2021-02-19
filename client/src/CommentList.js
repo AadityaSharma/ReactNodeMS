a0d4b5dfd492506339710dcb0bc1c1e51eb41da8
Aaditya Sharma <aaditya.sh20@gmail.com>
2021-02-19 22:49:42 +0530

    Updated services logic

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,7 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 export default ({ comments }) => { 
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>;
+        let content;
+
+        if (comment.status === 'approved') {
+            content = comment.content;
+        }
+
+        if (comment.status === 'pending') {
+            content = `Comment is awaiting moderation (id: ${comment.id})`;
+        }
+
+        if (comment.status === 'rejected') {
+            content = `Comment is rejected (id: ${comment.id})`;
+        }
+
+        return <li key={comment.id}>{content}</li>;
     });
 
     return (
--- event-bus/index.js
+++ event-bus/index.js
@@ -6,14 +6,17 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
+    console.log('Received Event', req.body.type);
+
     const event = req.body;
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
     axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4003/events', event);
 
     res.send({ status: 'OK' });
-})
+});
 
 app.listen(4005, () => {
     console.log('listening on 4005');
